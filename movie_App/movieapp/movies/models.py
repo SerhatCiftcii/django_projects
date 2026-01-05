e615df9c1d3e5420fd13e8c348695a1a832d76e1
Serhat <s.ciftci561@gmail.com>
2026-01-05 21:54:25 +0300

    migrations_modelsmovie

--- movie_App/movieapp/movies/models.py
+++ movie_App/movieapp/movies/models.py
@@ -1,23 +1,17 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+from django.db.models.fields import CharField
+ 
+
+
 class Genre(models.Model):
     name=models.CharField(max_length=100)
-    
 
-# Create your models here.
-class Movie(models.Model):
-    title=models.CharField(max_length=100)
-    description=models.TextField(validators=[MinLengthValidator(20)]) # mutlaka 20 karakter olmalı
-    image_name=models.CharField(max_length=50)
-    image_cover=models.CharField(max_length=50)
-    date = models.DateField(),
-    slug=models.SlugField(unique=True,db_index=True)
-    budget=models.DecimalField(max_digits=15,decimal_places=2)
-    language=models.CharField(max_length=30),
 class Contact(models.Model):
     address=models.CharField(max_length=200)
-    models.EmailField()
-class Person:
+    email=models.EmailField(null=True, blank=True)
+      
+class Person(models.Model):
     genders = (
         ('M', "Erkek"),
         ('F', "Kadın"),
@@ -36,3 +30,27 @@ class Person:
     date_of_birth=models.DateField()
     gender=models.CharField(max_length=1,choices=genders)
     duty_type=models.CharField(max_length=1, choices=duty_types)
+    contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True, blank=True)
+
+
+# Create your models here.
+class Movie(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.TextField(validators=[MinLengthValidator(20)]) # mutlaka 20 karakter olmalı
+    image_name=models.CharField(max_length=50)
+    image_cover=models.CharField(max_length=50)
+    date = models.DateField(),
+    slug=models.SlugField(unique=True,db_index=True)
+    budget=models.DecimalField(max_digits=15,decimal_places=2)
+    language=models.CharField(max_length=30)
+    people = models.ManyToManyField(Person)
+    genres = models.ManyToManyField(Genre)
+    
+
+    
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    url= models.CharField(max_length=200)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    
+    
