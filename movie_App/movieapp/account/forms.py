b813364f025ee5b666f10ac9b990223db71d7f26
Serhat <s.ciftci561@gmail.com>
2026-01-20 23:43:13 +0300

    update_

--- movie_App/movieapp/account/forms.py
+++ movie_App/movieapp/account/forms.py
@@ -1,5 +1,6 @@
 # öncedn usernama ve password vardı yada email burda email ve ekstra hiç djangoda olmayan rememberı buraya  eklceiz benım anladığım username passwrodn emaial hazır , ama remember yok
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
@@ -8,5 +9,9 @@ class LoginForm(forms.Form):
     remember_me=forms.BooleanField(required=False,initial=False, widget=forms.CheckboxInput(attrs={"class":"custom-control-input"})) # seçilmezse fals gelir initail o required bi alanda değil zaten
 
     def clean_email(self):
-        email=self.changed_data.get("email")
+        email=self.cleaned_data.get("email")
+        
+        if not User.objects.filter(email=email).exists():
+            self.add_error("email","Email ile kayıtlı kullanıcı yok")
+        return email
         
--- movie_App/movieapp/account/views.py
+++ movie_App/movieapp/account/views.py
@@ -3,6 +3,8 @@ from django.template.context_processors import request
 from django.contrib.admin import register
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+
+
 from account.forms import LoginForm
 # Create your views here.
 def login_request(request):
@@ -10,19 +12,20 @@ def login_request(request):
         return redirect("home_page")
     if request.method == "POST":
         form = LoginForm(request.POST)
+        
         if form.is_valid():
             email = form.cleaned_data.get("email")
             password = form.cleaned_data.get("password")
-            if User.objects.filter(email=email).exists(): # true gelirse veri tabanına  kayıt vardır  bi içeri alırız mailvarsada giriş olur yoksa boş formuj çıakrıız yada else  yzarak hata mesajıyla birlikte dönerderiiz
-                username= User.objects.get(email=email).username
-                user = authenticate(username=username, password=password)
-            else:
-                 return render(request, "account/login.html",{
-                    "form":form
-                })
+            remember_me=form.cleaned_data.get("remember_me")
+            username= User.objects.get(email=email).username
+            user = authenticate(username=username, password=password)
+           
             
             if user is not None:
                 login(request, user)
+                if not remember_me:
+                    request.session.set_expiry(0)
+                    request.session.modified=True
                 return redirect("home_page")
             else:
                 form.add_error(None, "email yada parola yanlıştır")
@@ -30,15 +33,16 @@ def login_request(request):
                     "form":form
                 })
         else:
-            form.add_error(None, "email ile kayıtlı bi kuallnıcı yoktur")
-            return render(request, "account/login.html",{
-                    "form":form
-                })
-
+            return render(request,"account/login.html",{
+                "form":form
+            })
+    
+    
     form = LoginForm()
     return render(request, "account/login.html",{
         "form":form
     })
+    
 def register_request(request):
     return render(request,"account/register.html")
 
